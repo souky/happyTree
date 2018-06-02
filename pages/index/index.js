c1cb5454e7e81b2a61277753a301d2ac5ca0ef29
souky <136069803@qq.com>
2018-06-02 11:48:20 +0800

    add

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,33 +1,67 @@
-
+var Crypto = require('cryptojs/cryptojs.js').Crypto;
 Page({
   data: {
     msg:""
   },
-  
+  onLoad: function () {
+   
+  },
+  onGotUserInfo : function(e){
+    console.log(e);
+  },
   wxLogins : function(e){
-    wx.login({
-      success: function (res) {
-        if (res.code) {
-          let code = res.code;
-          wx.request({
-            url: 'https://192.168.1.89:8080/happyTree/Wxlogin', //仅为示例，并非真实的接口地址
-            data: {
-              code: code
-            },
-            method:'POST',
-            success: function (res) {
-              console.log(res)
-            }
-          })
+    //console.log(e);
+    let detail = e.detail;
+    let dateDe = this.decryptData(detail.encryptedData,detail.iv,"");
+    console.log(dateDe);
+    // wx.login({
+    //   success: function (res) {
+    //     if (res.code) {
+    //       let code = res.code;
+    //       console.log(code)
+    //       wx.request({
+    //         url: 'https://192.168.1.89:8080/happyTree/Wxlogin', 
+    //         data: {
+    //           code: code
+    //         },
+    //         method:'POST',
+    //         success: function (res) {
+    //           console.log(res)
+    //         }
+    //       })
           
-        } else {
-          console.log('登录失败！' + res.errMsg)
-        }
-      }
-    });
+    //     } else {
+    //       console.log('登录失败！' + res.errMsg)
+    //     }
+    //   }
+    // });
   },
   defLogins : function(e){
     console.log(e);
+  },
+  decryptData:function(appDate,iv,sessionKey){
+    var encryptedData = Crypto.util.base64ToBytes(appDate)
+    var key = Crypto.util.base64ToBytes(sessionKey);
+    var iv = Crypto.util.base64ToBytes(iv);
+
+    // 对称解密使用的算法为 AES-128-CBC，数据采用PKCS#7填充
+    var mode = new Crypto.mode.CBC(Crypto.pad.pkcs7);
+    var decryptResult = "";
+    try {
+      // 解密
+      var bytes = Crypto.AES.decrypt(encryptedData, key, {
+        asBpytes: true,
+        iv: iv,
+        mode: mode
+      });
+
+      decryptResult = JSON.parse(bytes);
+
+    } catch (err) {
+      console.log(err)
+    }
+
+    return decryptResult
   }
    
 });
