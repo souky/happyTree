d29f15e86301a4d487cbf99fc64fb39532ca6d04
souky <136069803@qq.com>
2018-06-29 17:50:05 +0800

    friends

--- pages/friends/friends.js
+++ pages/friends/friends.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    uersList: [{ id: "1" }, { id: "2" }, { id: "3" }, { id: "4" }, { id: "5" }, { id: "6" }, { id: "7" }, { id: "8" }, { id: "9" }, { id: "10" }, { id: "11" }, { id: "12" }, { id: "13" }, { id: "14" }, { id: "15" }],
+    userShow:[]
   },
 
   /**
@@ -14,8 +15,39 @@ Page({
   onLoad: function (options) {
     wx.setNavigationBarTitle({
       title: '我的伙伴',
-    })
+    });
+    
+    this.initFri(0);
   },
-
+  //初始化朋友数据
+  initFri:function(index){
+    let list = this.data.uersList;
+    let new_list = new Array();
+    let len = list.length;
+    let offset = len - index;
+    let forIndex = 0
+    //断点加载
+    if(offset < 11){
+      forIndex = index - 11 + offset;
+    }
+    if (len > 12){
+      for (let x = forIndex;x < (forIndex+11);x++){
+        if(x == index){
+          list[x].active = 'active'
+        }else{
+          list[x].active = ''
+        }
+        new_list.push(list[x])
+      };
+      
+      this.setData({
+        userShow : new_list
+      })
+    }
+  },
+  bindPickerChange:function(e){
+    console.log(e)
+    this.initFri(e.detail.value);
+  }
   
 })
