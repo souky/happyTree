edd96dc9a631be75a2dff29d81c071e22d43a068
souky <136069803@qq.com>
2018-07-04 17:54:05 +0800

    home

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,24 +1,49 @@
 // pages/home/home.js
+const app = getApp();
 Page({
 
   data: {
     imgUrls: [
-      '../../img/home/swiper_1.jpg',
-      '../../img/home/swiper_2.jpg',
-      '../../img/home/swiper_3.jpg'
+      '../../img/home/swiper/swiper_1.png'
+      
     ],
     indicatorDots: true,
     autoplay: true,
     interval: 5000,
     duration: 1000,
-    activeColor:'#ffffff'
+    activeColor:'#ffffff',
+    rankList:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    wx.showTabBar();
+  onLoad: function () {
+    app.utils.wxpost("punchrank/getLastWeekRank","",res =>{
+      let list = res.result;
+      
+      for(let x in list){
+        if(list[x].id == undefined){
+          list[x].em = true;
+        }else{
+          list[x].em = false;
+        }
+        if(x == 0){
+          list[x].pngs = '../../img/home/silver.png'
+        }
+        if (x == 1) {
+          list[x].pngs = '../../img/home/gold.png'
+        }
+        if (x == 2) {
+          list[x].pngs = '../../img/home/copper.png'
+        }
+        
+      }
+      
+      this.setData({
+        rankList: list
+      })
+    })
    
   },
 
